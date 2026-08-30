b43bd17efd876e8cb781576a7434ffb4f46b89f4
louiebond1 <lbondboy@hotmail.co.uk>
2026-08-30 15:25:43 +0100

    Add offline service worker

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+const CACHE='kickpot-v1';
+const CORE=['/','/styles.css','/app.js','/manifest.webmanifest','/icons/icon-192.png','/icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||new URL(e.request.url).pathname.startsWith('/api/')) return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)));
+});
