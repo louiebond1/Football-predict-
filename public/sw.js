fdd9e674cc1354d589d9efa77c7883fd14e0fdee
louiebond1 <lbondboy@hotmail.co.uk>
2026-09-04 23:46:23 +0100

    Fix service worker activation syntax

--- public/sw.js
+++ public/sw.js
@@ -41,7 +41,7 @@ self.addEventListener('install',e=>{
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   self.clients.claim(),
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-]));
+])));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET'||new URL(e.request.url).pathname.startsWith('/api/')) return;
   e.respondWith(
